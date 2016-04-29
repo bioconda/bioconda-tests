047d5b386082b347dfd4861387816a2e39e66e85
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-04-29 20:24:15 +0200

    Configure travis to test all recipes.

---
+++ scripts/travis-run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+if [[ $TRAVIS_OS_NAME = "linux" ]]
+then
+    # run CentOS5 based docker container
+    docker run -e SUBDAG -e SUBDAGS -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`/bioconda-recipes:/bioconda-recipes bioconda/bioconda-builder --testonly
+    # Build package documentation
+    ./scripts/build-docs.sh
+else
+    export PATH=/anaconda/bin:$PATH
+    # build packages
+    scripts/build-packages.py --testonly --repository bioconda-recipes
+fi
