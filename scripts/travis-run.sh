b51e0c15fc29a2e78a5f8ebfc72182f077049002
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-04-30 13:36:44 +0200

    Specify env-matrix.

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
     # run CentOS5 based docker container
-    docker run -e SUBDAG -e SUBDAGS -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`/bioconda-recipes:/bioconda-recipes bioconda/bioconda-builder --testonly
+    docker run -e SUBDAG -e SUBDAGS -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`/bioconda-recipes:/bioconda-recipes bioconda/bioconda-builder --testonly --env-matrix /bioconda-recipes/scripts/env_matrix.yml
 else
     export PATH=/anaconda/bin:$PATH
     # build packages
-    scripts/build-packages.py --testonly --repository bioconda-recipes
+    scripts/build-packages.py --testonly --repository bioconda-recipes --env-matrix bioconda-recipes/scripts/env_matrix.yml
 fi
