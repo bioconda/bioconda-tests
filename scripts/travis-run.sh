e73be7a0b8987188f29e46e3b179743ca20aa1f0
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-04-30 10:32:23 +0200

    Remove superfluous code.

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -5,8 +5,6 @@ if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
     # run CentOS5 based docker container
     docker run -e SUBDAG -e SUBDAGS -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`/bioconda-recipes:/bioconda-recipes bioconda/bioconda-builder --testonly
-    # Build package documentation
-    ./scripts/build-docs.sh
 else
     export PATH=/anaconda/bin:$PATH
     # build packages
